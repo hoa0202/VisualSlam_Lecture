f639260247534356affa17ab86d680f9fee67a81
hoa0202 <hoa0202@naver.com>
2023-04-03 11:14:23 +0900

    Revert "Create day1_1_rot90.py"
    change the step

--- day1_2_rot90.py
+++ day1_2_rot90.py
@@ -5,7 +5,7 @@ theta = np.radians(-90) #시계방향
 c, s = np.cos(theta), np.sin(theta)
 r = np.array(((c, -s), (s, c)))
 t = np.array((1,0))
-v = np.array((0,1))
+v = np.array((0,1)) + t
 plt.plot(v[0], v[1], "rs") # 사각형
-plt.plot(r.dot(v)[0]+t[0], r.dot(v)[1]+t[1], 'bo') #파란색 동그라미
+plt.plot(r.dot(v)[0], r.dot(v)[1], 'bo') #파란색 동그라미
 plt.show()
