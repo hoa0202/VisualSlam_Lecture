6f0d6d4ff4f7a4a7e0f4505384fead2e6bcc2c6d
hoa0202 <hoa0202@naver.com>
2023-04-03 11:09:05 +0900

    Create day1_1_rot90.py
    add

---
+++ day1_1_rot90.py
@@ -0,0 +1,10 @@
+# day1_1_rot90.py
+import numpy as np
+import matplotlib.pyplot as plt
+theta = np.radians(-90) #시계방향
+c, s = np.cos(theta), np.sin(theta)
+r = np.array(((c, -s), (s, c)))
+v = np.array((0,1))
+plt.plot(v[0], v[1], "rs") # 사각형
+plt.plot(r.dot(v)[0], r.dot(v)[1], 'bo') #파란색 동그라미
+plt.show()
